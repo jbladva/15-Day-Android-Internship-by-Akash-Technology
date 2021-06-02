fb6bd5881c24e86ce1782f6ae3f0f112ca94f403
jbladva <jaydeepladva741@gmail.com>
2021-06-02 19:48:23 +0530

    Day 6 commit

---
+++ Day_6/Splash Screen/Java/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.splashscreen;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+
+}
---
+++ Day_6/Splash Screen/Java/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.example.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        Thread thread = new Thread(){
+
+
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this , MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+        thread.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+
+
+}
