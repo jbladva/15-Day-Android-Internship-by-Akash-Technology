255022229804d263d002dcd4d97063df9bfd8727
jbladva <jaydeepladva741@gmail.com>
2021-06-04 12:10:40 +0530

    day 8 commit

---
+++ Day_8/Java/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.customelistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    int[] image = {R.drawable.beer,R.drawable.cat,R.drawable.dog,R.drawable.lion,R.drawable.monkey,R.drawable.peacock,R.drawable.tiger,R.drawable.zebra};
+    String[] text = {"This is beer","This is cat","This is dog","This is lion","This is monkey","This is peacock","This is tiger","This is zebra"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview);
+        myAdapter myadapter = new myAdapter(MainActivity.this ,image,text);
+        listView.setAdapter(myadapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this , displayData.class);
+                intent.putExtra("image",image[position]);
+                intent.putExtra("text",text[position]);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
